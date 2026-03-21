1d6413a960e34ead4a9b6a450f679ddf0ea08d3d
Alexandr Musin <poolce@list.ru>
2026-03-21 18:11:12 +0300

    Implement execution module

--- app/execution/execution_module.h
+++ app/execution/execution_module.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "module.h"
+
+namespace Emulator
+{
+class ExecutionModule
+{
+  private:
+    std::shared_ptr<Ptx::Module> module_;
+
+  public:
+    ExecutionModule(const std::shared_ptr<Ptx::Module>& module) : module_(module) {}
+};
+
+} // namespace Emulator
