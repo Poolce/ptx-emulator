b48b356cc5366771aee53b74ce0706d99a91d030
Kalinin Eugene <ujinval@yandex.ru>
2026-04-18 08:55:39 +0300

    Improve CUDA samples testing (#24)
    - Automatic preloaded lib path setting. Setting `LD_LIBRARY_PATH` is
      nomore needed.
      Assumes the libs are located next to bins in separate folder. If
      library path is not default, user can still specify it by setting
      `LD_LIBRARY_PATH``.
    - Added CMake option for compiling provided CUDA samples.
    
    Co-authored-by: Kalinin Eugene <ujival@yandex.ru>

--- app/emulator/main.cpp
+++ app/emulator/main.cpp
@@ -1,6 +1,32 @@
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include <string>
+
+namespace fs = std::filesystem;
+
+constexpr const char* RuntimeLibName = "libemuruntime.so";
+
+static fs::path GetExecutableDir()
+{
+    std::error_code ec;
+    auto exe_path = fs::read_symlink("/proc/self/exe", ec);
+    if (!ec)
+    {
+        auto dir = exe_path.parent_path();
+        if (!dir.empty())
+        {
+            return dir;
+        }
+    }
+    return ".";
+}
+
+static fs::path GetLibDir()
+{
+    fs::path lib_path = GetExecutableDir() / ".." / "lib";
+    return lib_path.lexically_normal();
+}
 
 int main(int argc, char* argv[])
 {
@@ -9,10 +35,14 @@ int main(int argc, char* argv[])
         std::cerr << "Usage: " << argv[0] << " <binary>\n";
         return 1;
     }
-    std::string file = argv[1];
-    auto cmd = std::string("LD_PRELOAD=libemuruntime.so CUEMU_TARGET_EXEC=");
+    const std::string file = argv[1];
+
+    std::string cmd;
+    cmd += "PATH=" + GetExecutableDir().string() + ":$PATH" + " ";
+    cmd += "LD_LIBRARY_PATH=" + GetLibDir().string() + ":$LD_LIBRARY_PATH" + " ";
+    cmd += "LD_PRELOAD=" + std::string(RuntimeLibName) + " ";
+    cmd += "CUEMU_TARGET_EXEC=" + file + " ";
     cmd += file;
-    cmd += " ";
-    cmd += file;
+
     return system(cmd.c_str());
 }
